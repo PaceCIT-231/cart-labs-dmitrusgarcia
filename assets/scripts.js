72479fde9abc4ee93e975a993bedfc665abfb3a4
Dmitrus <dg41348p@pace.edu>
2022-11-16 15:04:25 -0500

    lab 9 change

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,6 +15,9 @@ function addToCart(cookie) {
    itemCount = itemCount+1
     console.log(itemCount)
 
+    //print itemcount to cartItems id
+    document.getElementById("cartItems").innerText = itemCount
+
    //add the correct price to the currentPrice variable
    if (cookie == 'peanut butter') {
     currentPrice = currentPrice + 20
@@ -28,13 +31,27 @@ function addToCart(cookie) {
     else if (cookie == 'chocolate chip') {
     currentPrice = currentPrice + 25
     }
+
 console.log(currentPrice)
+//print currentPrice from if statement to hovertext id
+document.querySelector(".hoverText").innerHTML = "$" + currentPrice
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
     
-    window.alert(`You have ${itemCount} items that will cost $${currentPrice}.  Thank you for shopping with us!`)
+    prompt(`You have ${itemCount} items that will cost $${currentPrice}.  Please provide your Name and Address in the prompt below for your bill.\n` + '\nThank you for shopping with us!')
 
+    currentPrice = 0
+    itemCount = 0
+
+    document.querySelector(".hoverText").innerHTML = "$" + currentPrice
+    document.getElementById("cartItems").innerText = itemCount
 }
+
+function darkMode() {
+    document.querySelector("body").style.backgroundColor = "black"
+    document.querySelector("main").style.color = "white"
+    document.querySelector("header").style.color = "white"
+} 
